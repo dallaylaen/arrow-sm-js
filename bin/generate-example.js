5f4ad49978c873c15cf87e6cfdc67c1c4cf293b6
Konstantin S. Uvarin <khedin@gmail.com>
2019-10-30 23:54:07 +0200

    EXA Example generator can actually switch something

--- bin/generate-example.js
+++ bin/generate-example.js
@@ -3,6 +3,10 @@
 'use strict';
 const pug = require('pug');
 
+function str(x) {
+    return typeof x === 'string' ? "'"+x+"'" : x;
+};
+
 const fun = pug.compile(`
 mixin example(item)
     div(class='example' id='example-'+item.id)
@@ -18,16 +22,25 @@ mixin example(item)
         div(id='log'+item.id)
         script
             |
-            | const sm#{item.id} = new ArrowSM()
+            | var sm#{item.id} = new ArrowSM()
             |   .onSwitch( function (e,o,n) {
-            |       getElementById('log#{item.id}').innerHTML =        
-            | }
-            | // TODO declare states
-            | .start(#{item.initial});
+            |       document.getElementById('log#{item.id}').innerHTML = o+' -> '+n+' via '+e;
+            |   })
+            each state in item.states 
+                | .addState( #{typeof state.name === string ? "'"+state.name+"'" : state.name }, {
+                each entry in ['decide', 'enter', 'leave' ]
+                    if state[entry]
+                        | #{entry} : #{state[entry]},
+                | })
+                |
+            |   .start(#{item.initial});
 html
     head
+        meta(http-equiv="Content-Type" content="text/html; charset=utf-8")
         title #{title}
         script(src='js/arrow-sm.js')
+        script
+            | function $(str) { return document.getElementById(str) }
     body
         h1 #{title}
 
@@ -42,13 +55,13 @@ const examples = [
         name: 'Foo bar',
         descr: 'Lorem ipsum wtf there was',
         states: [
-            { name: true, descr: 'Default toggle switch' },
-            { name: false, descr: 'Other toggle switch' },
+            { name: true, descr: 'Default toggle switch', decide: 'function() { return false}' },
+            { name: false, descr: 'Other toggle switch', decide: 'function() { return true}' },
         ],
         events: [
             'toggle',
         ],
-        intial: true,
+        initial: true,
     },
 ].map((x,i)=>1&&{ id: i+1, ...x });
 
